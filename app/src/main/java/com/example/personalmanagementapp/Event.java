c2dd6d238fc183e8893881f9e33927086def728b
Eric Njeru <ericmukundi10@gmail.com>
2020-09-10 16:29:50 +0300

    Db Helper methods

---
+++ app/src/main/java/com/example/personalmanagementapp/Event.java
@@ -0,0 +1,52 @@
+package com.example.personalmanagementapp;
+
+import java.io.Serializable;
+
+public class Event implements Serializable {
+
+    private int id;
+    private String name;
+    private String date;
+    private String time;
+    private String location;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ app/src/main/java/com/example/personalmanagementapp/FriendPhoto.java
@@ -0,0 +1,34 @@
+package com.example.personalmanagementapp;
+
+import java.io.Serializable;
+
+public class FriendPhoto implements Serializable {
+
+    private int id;
+    private int friend_id;
+    private int photo_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFriend_id() {
+        return friend_id;
+    }
+
+    public void setFriend_id(int friend_id) {
+        this.friend_id = friend_id;
+    }
+
+    public int getPhoto_id() {
+        return photo_id;
+    }
+
+    public void setPhoto_id(int photo_id) {
+        this.photo_id = photo_id;
+    }
+}
